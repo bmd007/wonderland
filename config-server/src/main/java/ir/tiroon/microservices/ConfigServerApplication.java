b17565a8ce1db34fe30fabd6573740621be9bf10
bmd007 <bmd579@gmail.com>
2018-06-09 12:31:43 +0430

    config server addition in progress

--- config-server/src/main/java/ir/tiroon/microservices/ConfigServerApplication.java
+++ config-server/src/main/java/ir/tiroon/microservices/ConfigServerApplication.java
@@ -1,11 +1,13 @@
 package ir.tiroon.microservices;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
