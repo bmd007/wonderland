63bff2057e9ec941b43b10fd21bb6eb534dfff8d
bmd007 <bmd579@gmail.com>
2019-04-17 15:14:12 +0200

    some stuff

--- gateway/src/main/groovy/it/tiroon/microservices/gateway/GatewayApplication.groovy
+++ gateway/src/main/groovy/it/tiroon/microservices/gateway/GatewayApplication.groovy
@@ -2,9 +2,31 @@ package it.tiroon.microservices.gateway
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator
+import org.springframework.context.annotation.Bean
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
+import org.springframework.security.config.web.server.ServerHttpSecurity
+import org.springframework.security.web.server.SecurityWebFilterChain
 
+//@EnableWebFluxSecurity
 @SpringBootApplication
 class GatewayApplication {
+
+
+//	@Bean
+	SecurityWebFilterChain authorization(ServerHttpSecurity security){
+		security
+			.authorizeExchange().pathMatchers("/secured").authenticated()
+			.anyExchange().permitAll()
+			.and().httpBasic()
+			.and().build()
+	}
+
+	@Bean
+	DiscoveryClientRouteDefinitionLocator definitionLocator(DiscoveryClient discoveryClient){
+		return new DiscoveryClientRouteDefinitionLocator(discoveryClient)
+	}
 
 	static void main(String[] args) {
 		SpringApplication.run(GatewayApplication, args)
