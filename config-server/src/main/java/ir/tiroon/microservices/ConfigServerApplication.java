6b56143f08ee06cdf4195365ce26b8a5f81f2cc8
bmd007 <bmd579@gmail.com>
2019-04-17 15:14:41 +0200

    more stuff

--- config-server/src/main/java/ir/tiroon/microservices/ConfigServerApplication.java
+++ config-server/src/main/java/ir/tiroon/microservices/ConfigServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-@EnableAutoConfiguration
 public class ConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigServerApplication.class, args);
