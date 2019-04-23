b8894c2a3563f582793d8b4ee44999d657bc44bf
bmd007 <bmd579@gmail.com>
2019-04-23 18:13:53 +0200

    khkh

--- video-streamer-rscoket/src/main/java/ir/tiroon/microservices/videostreamer/VideoStreamerRsocketApplication.java
+++ video-streamer-rscoket/src/main/java/ir/tiroon/microservices/videostreamer/VideoStreamerRsocketApplication.java
@@ -1,11 +1,16 @@
 package ir.tiroon.microservices.videostreamer;
 
+import ch.qos.logback.core.encoder.ByteArrayUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.FileSystemResourceLoader;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.io.*;
+import java.nio.file.Files;
 
 @SpringBootApplication
 public class VideoStreamerRsocketApplication {
@@ -19,11 +24,23 @@ public class VideoStreamerRsocketApplication {
 @Controller
 class VideoStreamerController {
 
+
+
     @MessageMapping("xxx")
-    public Flux<Byte> bytesOfXXX(Mono<String> username) {
+    public Flux<Byte> bytesOfXXX(Mono<String> username) throws IOException {
+        File file = new File("src/main/resources/abc.pdf");
+        byte[] bytes = Files.readAllBytes(file.toPath());
+
+        Byte[] byteObjects = new Byte[bytes.length];
+        int i=0;
+        for(byte b: bytes) {
+            byteObjects[i++] = b;
+        }
+
+        Flux<Byte> byteFlux = Flux.fromArray(byteObjects);
         return username.filter(s -> s.equals("bmd"))
                 .switchIfEmpty(Mono.error(new RuntimeException("Not Found")))
-                .flatMapMany();
+                .flatMapMany(s -> byteFlux);
     }
 }
 //**TODO
