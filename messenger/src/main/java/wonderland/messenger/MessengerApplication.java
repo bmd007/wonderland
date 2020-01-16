730c7097d98ca0fd547b64dd052bd890cdc4e0ab
bmd007 <bmd579@gmail.com>
2020-01-16 20:34:28 +0100

    successfull connection to kafka and separation of default profile from docker-compose profile

--- messenger/src/main/java/wonderland/messenger/MessengerApplication.java
+++ messenger/src/main/java/wonderland/messenger/MessengerApplication.java
@@ -6,10 +6,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
+@EnableKafka
 @SpringBootApplication
 public class MessengerApplication {
 
@@ -18,4 +24,5 @@ public class MessengerApplication {
     }
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MessengerApplication.class);
+
 }
