211062204e4a6b779fd74e857ad69de96966ef4d
bmd007 <bmd579@gmail.com>
2020-01-24 16:51:42 +0100

    graph custom query works

--- communication_graph/src/main/java/wonderland/communication/graph/repository/PersonRepository.java
+++ communication_graph/src/main/java/wonderland/communication/graph/repository/PersonRepository.java
@@ -6,4 +6,9 @@ import wonderland.communication.graph.domain.Person;
 
 @Repository
 public interface PersonRepository extends Neo4jRepository<Person, String> {
+//    CALL algo.pageRank.stream("Person", "SENT_MESSAGE", {iterations:20})
+//    YIELD nodeId, score
+//    MATCH (node) WHERE id(node) = nodeId
+//    RETURN node.email AS page,score
+//    ORDER BY score DESC
 }
