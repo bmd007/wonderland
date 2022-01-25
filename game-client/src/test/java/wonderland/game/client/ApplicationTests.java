227f913f9eab29a47a9fce465ee7ac0be4b18515
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-01-25 11:46:10 +0100

    game client experiment

--- game-client/src/test/java/wonderland/game/client/ApplicationTests.java
+++ game-client/src/test/java/wonderland/game/client/ApplicationTests.java
@@ -1,9 +1,12 @@
-package wonderland.wiseman;
+package wonderland.game.client;
 
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @SpringBootTest
 @ActiveProfiles("test")
