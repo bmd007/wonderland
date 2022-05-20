8332d338766747c27fdb4bc6a4bd2896b8d6e23c
Mahdi Amini <bmd579@gmail.com>
2022-05-20 21:28:39 +0200

    resolving sneakymail problme

--- wonder-matcher/src/main/java/wonderland/wonder/matcher/config/KafkaStreamsDefaultConfig.java
+++ wonder-matcher/src/main/java/wonderland/wonder/matcher/config/KafkaStreamsDefaultConfig.java
@@ -2,6 +2,7 @@ package wonderland.wonder.matcher.config;
 
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.errors.LogAndContinueExceptionHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,9 +10,12 @@ import org.springframework.core.env.Environment;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaStreamsDefaultConfiguration;
 import org.springframework.kafka.config.KafkaStreamsConfiguration;
+import org.apache.kafka.streams.StreamsConfig;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @EnableKafkaStreams
@@ -22,34 +26,37 @@ public class KafkaStreamsDefaultConfig {
     public KafkaStreamsConfiguration kafkaStreamsConfig(
             @Value("${spring.application.name}") String applicationName,
             @Value("${spring.kafka.bootstrap-servers}") String bootStrapServers,
-            @Value("${kafka.streams.replication-factor:1}") int replicationFactor,
-            @Value("${kafka.streams.properties.num.stream.threads:1}") int numStreamThreads,
-            @Value("${kafka.streams.server.config.app.port:9585}") int port,
-            @Value("${kafka.streams.server.config.app.ip:localhost}") String ip,
+            @Value("${kafka.streams.server.config.app-ip}") String ip,
+            @Value("${kafka.streams.server.config.app-port}") String port,
+            @Value("${spring.kafka.streams.replication-factor}") int kafkaStreamsReplicationFactor,
             Environment environment) {
-        var props = new HashMap<String, Object>();
-
-        props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName);
+        Map<String, Object> props = new HashMap<>();
+        // equivalent to group-id
         props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServers);
-        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, replicationFactor);
-        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, numStreamThreads);
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, applicationName);
+        props.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
+        props.put(StreamsConfig.CLIENT_ID_CONFIG, applicationName + "-" + UUID.randomUUID());
 
         // Using this means accepting the app to continue when it faces a deserialization error, instead of break down
-//        props.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, LogAndContinueExceptionHandler.class);
+        props.put(StreamsConfig.DEFAULT_DESERIALIZATION_EXCEPTION_HANDLER_CLASS_CONFIG, LogAndContinueExceptionHandler.class);
 
-        //        // This configuration is for making remote interactive queries possible
+        // this is needed
+        props.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+
+        // This configuration is for making remote interactive queries possible
         props.put(StreamsConfig.APPLICATION_SERVER_CONFIG, ip + ":" + port);
+        props.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, kafkaStreamsReplicationFactor);
 
         var activeProfiles = Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toSet());
-        //The properties below, should not be applied when active profile is test
-        if (!activeProfiles.contains("test") && !activeProfiles.contains("local")) {
+        //The properties below, should not be applied when active profile is test or mock
+        //But if the active profiles include docker-compose-all, they should be applied regardless of other active profiles.
+        if (!activeProfiles.contains("test")) {
             props.put(StreamsConfig.PROCESSING_GUARANTEE_CONFIG, StreamsConfig.EXACTLY_ONCE);
         } else {
             props.put(StreamsConfig.COMMIT_INTERVAL_MS_CONFIG, 100); //to slow down commits so that tests can catch up with the eventual consistency ?>!
         }
-        //TODO investigate usage of        cache.max.bytes.buffering
-        //TODO investigate usage of         state.cleanup.delay.ms
+//TODO investigate usage of        cache.max.bytes.buffering
+//TODO investigate usage of         state.cleanup.delay.ms
         return new KafkaStreamsConfiguration(props);
     }
 
