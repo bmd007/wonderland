4349779b333ed21d34db030fc260d80bc495f95a
Mahdi Amini <bmd579@gmail.com>
2022-05-22 15:55:30 +0200

    api gateway rsocket

--- api-gateway/src/main/java/wonderland/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/wonderland/api/gateway/ApiGatewayApplication.java
@@ -2,12 +2,29 @@ package wonderland.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.time.Duration;
+
+@Controller
 @SpringBootApplication
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@MessageMapping("stream")
+	Flux<Long> numbers(){
+		return Flux.interval(Duration.ofSeconds(1));
+	}
+
+	@MessageMapping("echo")
+	Mono<String> echo(String text){
+		return Mono.just(text);
 	}
 
 }
