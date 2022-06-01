8cb0c1dbec80c9b2e03a1d6c9e0feba7910063b0
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-06-01 15:12:51 +0200

    location package for flutter

--- dance_partner_finder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dance_partner_finder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers
+import location
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersPlugin.register(with: registry.registrar(forPlugin: "AudioplayersPlugin"))
+  LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
