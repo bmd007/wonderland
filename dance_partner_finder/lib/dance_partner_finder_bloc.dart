e0742366b9ea2b058a616b5c9d57cbe5d45a1de5
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-06-01 16:24:57 +0200

    location package for flutter

--- dance_partner_finder/lib/dance_partner_finder_bloc.dart
+++ dance_partner_finder/lib/dance_partner_finder_bloc.dart
@@ -3,7 +3,6 @@ import 'package:location/location.dart';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
-import 'package:rxdart/rxdart.dart';
 
 part 'dance_partner_finder_event.dart';
 part 'dance_partner_finder_state.dart';
