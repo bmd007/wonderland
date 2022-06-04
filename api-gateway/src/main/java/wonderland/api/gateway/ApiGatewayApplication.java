a694ed8722a9fe3c5ac108119fe89554acc09655
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-06-04 11:57:37 +0200

    like and dislike backend

--- api-gateway/src/main/java/wonderland/api/gateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/wonderland/api/gateway/ApiGatewayApplication.java
@@ -7,31 +7,71 @@ import org.springframework.stereotype.Controller;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Controller
 @SpringBootApplication
 public class ApiGatewayApplication {
 
-	private static ArrayList<String> names = new ArrayList<>();
+    private static ArrayList<String> potentialDancePartners = new ArrayList<>();
+    private static Map<String, Map<String, LocalDateTime>> likedDancers = new HashMap<>();
+    private static Map<String, Map<String, LocalDateTime>> disLikedDancers = new HashMap<>();
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 
-	@MessageMapping("names")
-	public Flux<String> numbers(){
-		return Flux.fromIterable(names);
-	}
+    @MessageMapping("names")
+    public Flux<String> names(String dancerPartnerSeekerName) {
+        var likedDancersByPartnerSeeker = Optional.ofNullable(likedDancers.get(dancerPartnerSeekerName))
+                .map(Map::entrySet)
+                .map(Set::stream)
+                .orElseGet(() -> Stream.empty());
+        var disLikedDancersByPartnerSeeker = Optional.ofNullable(disLikedDancers.get(dancerPartnerSeekerName))
+                .map(Map::entrySet)
+                .map(Set::stream)
+                .orElseGet(() -> Stream.empty());
+        var alreadySeenDancersBySeeker = Stream.concat(likedDancersByPartnerSeeker, disLikedDancersByPartnerSeeker).collect(Collectors.toSet());
 
-	@MessageMapping("addName")
-	public Mono<Void> addName(String name){
-		return Mono.just(name)
-				.doOnNext(names::add)
-				.log()
-				.then();
-	}
+        return Flux.fromIterable(potentialDancePartners)
+                .filter(dancerName -> );
+    }
+
+    @MessageMapping("addName")
+    public Mono<Void> addName(String name) {
+        return Mono.just(name)
+                .doOnNext(potentialDancePartners::add)
+                .log()
+                .then();
+    }
+
+    @MessageMapping("like")
+    public Mono<Void> likeADancer(String whoHasLiked, String whomIsLiked) {
+        var newLikee = Stream.of(Map.entry(whomIsLiked, LocalDateTime.now()));
+        var alreadyLikeLikeesStream = Optional.ofNullable(likedDancers.get(whoHasLiked))
+                .orElseGet(() -> Map.of()).entrySet().stream();
+        Map<String, LocalDateTime> newLikeesMap = Stream.concat(newLikee, alreadyLikeLikeesStream)
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+        likedDancers.put(whoHasLiked, newLikeesMap);
+        return Mono.empty();
+    }
+
+    @MessageMapping("disLike")
+    public Mono<Void> disLikeADancer(String whoHasDisLiked, String whomIsDisLiked) {
+        var newDisLikee = Stream.of(Map.entry(whoHasDisLiked, LocalDateTime.now());
+        var alreadyLikeLikeesStream = Optional.ofNullable(disLikedDancers.get(whomIsDisLiked))
+                .orElseGet(() -> Map.of()).entrySet().stream();
+        var newLikeesMap = Stream.concat(newDisLikee, alreadyLikeLikeesStream)
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
+        disLikedDancers.put(whoHasDisLiked, newLikeesMap);
+        return Mono.empty();
+    }
 
 }
