d08e2a1f4d38983f467e3224996c95ac5a055a4b
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-06-11 19:18:05 +0200

    mac os location done'

--- dance_partner_finder/lib/bloc/dance_partner_finder/dance_partner_finder_bloc.dart
+++ dance_partner_finder/lib/bloc/dance_partner_finder/dance_partner_finder_bloc.dart
@@ -8,17 +8,26 @@ part 'dance_partner_finder_event.dart';
 part 'dance_partner_finder_state.dart';
 
 
-Future<LocationData> getLocation() async {
+Stream<LocationData> getLocation() {
   Location location = Location();
 
-  if (!await location.serviceEnabled() && !await location.requestService()) {
-    return Future.error(Error());
-  }
-  if (await location.hasPermission() == PermissionStatus.denied &&
-      await location.requestPermission() != PermissionStatus.granted) {
-    return Future.error(Error());
-  }
-  return await location.getLocation();
+  return location.requestService()
+    .asStream()
+    .doOnData((event) => print("request service result: ${event}"))
+    .asyncMap((value) => location.requestPermission())
+    .doOnData((event) => print("request permission result: ${event}"))
+    .asyncMap((value) => location.getLocation())
+    .doOnData((event) => print("request location result: ${event}"))
+    .doOnError((p0, p1) => print("location error: ${p0}:${p1}"),);
+
+  // if (!await location.serviceEnabled() && !await location.requestService()) {
+  //   return Future.error(Error());
+  // }
+  // if (await location.hasPermission() == PermissionStatus.denied &&
+  //     await location.requestPermission() != PermissionStatus.granted) {
+  //   return Future.error(Error());
+  // }
+  // return await location.getLocation();
 }
 
 class DancePartnerBloc extends Bloc<DancePartnerEvent, DancePartnerState> {
@@ -30,7 +39,6 @@ class DancePartnerBloc extends Bloc<DancePartnerEvent, DancePartnerState> {
       emit(state.withThisDancerName(event.thisDancerName));
 
       getLocation()
-      .asStream()
       .doOnData((location) => client.addName(state.thisDancerName, location.latitude!, location.longitude! ))
       .asyncExpand((location) => client.fetchNames(state.thisDancerName, location.latitude!, location.longitude!))
       .forEach((potentialDancePartner) => add(PotentialDancerPartnerFoundEvent(potentialDancePartner!)));
