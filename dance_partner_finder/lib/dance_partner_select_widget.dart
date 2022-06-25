4b5df77739d4c5d7188d58045dc0eb597e6a325c
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-06-25 12:58:13 +0200

    firebase integration

---
+++ dance_partner_finder/lib/dance_partner_select_widget.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'bloc/dance_partner_finder/dance_partner_finder_bloc.dart';
+import 'bloc/dance_partner_finder/dance_partner_finder_event.dart';
+import 'dance_partner_matches_page.dart';
+
+class DancePartnerSelectWidget extends StatelessWidget {
+  DancePartnerSelectWidget({Key? key}) : super(key: key);
+
+  final _thisDancerNamTextController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    var dancerBloc = context.watch<DancePartnerFinderBloc>();
+    return Scaffold(
+      appBar: appBar(dancerBloc),
+      bottomNavigationBar: bottomNavigationBar(dancerBloc, context),
+      body: body(dancerBloc),
+    );
+  }
+
+  NavigationBar? bottomNavigationBar(DancePartnerFinderBloc dancerBloc, BuildContext context) {
+    return dancerBloc.state.thisDancerName.isNotEmpty
+        ? NavigationBar(
+            destinations: [
+              IconButton(
+                onPressed: () => Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            DancePartnerMatchesWidget(thisDancerName: dancerBloc.state.thisDancerName))),
+                icon: Image.asset(
+                  'images/match.gif',
+                  height: 40,
+                  width: 40,
+                ),
+              ),
+              Image.asset(
+                'images/match.png',
+                height: 40,
+                width: 40,
+              ),
+            ],
+            height: 40,
+          )
+        : null;
+  }
+
+  Widget body(DancePartnerFinderBloc dancerBloc) {
+    return dancerBloc.state.thisDancerName.isNotEmpty && !dancerBloc.state.isLoading
+        ? Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset('images/${dancerBloc.state.getCurrentDancerName()}.png', fit: BoxFit.fitHeight),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    dancerBloc.state.getCurrentDancerName(),
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      IconButton(
+                        onPressed: () => dancerBloc.add(DancerDislikedEvent(dancerBloc.state.getCurrentDancerName())),
+                        iconSize: 100,
+                        icon: Image.asset('images/panda.gif'),
+                      ),
+                      IconButton(
+                        onPressed: () => dancerBloc.add(DancerLikedEvent(dancerBloc.state.getCurrentDancerName())),
+                        iconSize: 150,
+                        icon: Image.asset('images/dancer.png'),
+                      ),
+                    ],
+                  )
+                ],
+              )
+            ],
+          )
+        : Image.asset('images/wait.png');
+  }
+
+  AppBar? appBar(DancePartnerFinderBloc dancerBloc) {
+    return dancerBloc.state.thisDancerName.isEmpty
+        ? AppBar(
+            title: TextField(
+              controller: _thisDancerNamTextController,
+            ),
+            actions: [
+                TextButton(
+                  onPressed: () => dancerBloc.add(ThisDancerChoseNameEvent(_thisDancerNamTextController.text)),
+                  child: const Text("touch after naming", style: TextStyle(color: Colors.black)),
+                )
+              ])
+        : null;
+  }
+}
--- dance_partner_finder/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dance_partner_finder/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,15 @@ import FlutterMacOS
 import Foundation
 
 import audioplayers_darwin
+import firebase_auth
+import firebase_core
 import location
 import path_provider_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   LocationPlugin.register(with: registry.registrar(forPlugin: "LocationPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
