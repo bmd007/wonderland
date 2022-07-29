1ef63470d1fb5c3cf5c95f4df4bd9802d4f46a49
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-07-29 15:05:29 +0200

    profile edit widget

--- dance_partner_finder/lib/dancer_profile_widget.dart
+++ dance_partner_finder/lib/dancer_profile_widget.dart
@@ -16,17 +16,16 @@ class DanceProfileWidget extends StatelessWidget {
       create: (context) => ProfileEditBloc(),
       child: BlocBuilder<ProfileEditBloc, ProfileEditState>(
         builder: (context, state) {
+          var profileEditBloc = context.watch<ProfileEditBloc>();
           if (state.isLoading) {
             return Image.asset('images/wait.png');
           }
           return Scaffold(
-              appBar: AppBar(
-                  actions: [
-                    TextButton(
-                      onPressed: () =>
-                          IconButton(
-                            onPressed: () =>
-                                Navigator.push(context,
+              appBar: AppBar(actions: [
+                TextButton(
+                  onPressed: () => IconButton(
+                    onPressed: () =>
+                        Navigator.push(context,
                                     MaterialPageRoute(builder: (context) => DancePartnerSelectWidget())),
                             icon: Image.asset(
                               'images/match.gif',
@@ -34,14 +33,47 @@ class DanceProfileWidget extends StatelessWidget {
                               width: 40,
                             ),
                           ),
-                      child: const Text("start matching", style: TextStyle(color: Colors.black)),
-                    )
-                  ])
-              ,
-              body:
-          );
+                  child: const Text("start matching", style: TextStyle(color: Colors.black)),
+                )
+              ]),
+              body: body(profileEditBloc, loginCubit));
         },
       ),
     );
+  }
+
+  Widget body(ProfileEditBloc profileEditBloc, LoginCubit loginCubit) {
+    return loginCubit.state.email.isNotEmpty && !profileEditBloc.state.isLoading
+        ? Stack(
+            fit: StackFit.expand,
+            children: [
+              Image.asset('images/${loginCubit.state.email}.png', fit: BoxFit.fitHeight),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    loginCubit.state.email,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                    ),
+                  ),
+                  Text(
+                    loginCubit.state.name,
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 50,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          )
+        : Image.asset('images/wait.png');
   }
 }
