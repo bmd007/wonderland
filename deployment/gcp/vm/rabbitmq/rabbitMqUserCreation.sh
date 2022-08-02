3b0ed5a1f3104fc0911c45efcaeca82a00c0a191
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-08-02 12:15:17 +0200

    web stomp

--- deployment/gcp/vm/rabbitmq/rabbitMqUserCreation.sh
+++ deployment/gcp/vm/rabbitmq/rabbitMqUserCreation.sh
@@ -5,4 +5,3 @@ printenv
 rabbitmqctl add_user rabbit-mq-web-stomp-credentials rabbit-mq-web-stomp-credentials
 rabbitmqctl set_permissions -p / rabbit-mq-web-stomp-credentials ".*" ".*" ".*"
 rabbitmqctl set_user_tags rabbit-mq-web-stomp-credentials management
-#this user is named mqtt but it actually doesn't have anything to do with mqtt
