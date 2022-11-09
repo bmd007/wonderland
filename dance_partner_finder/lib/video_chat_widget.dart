8def7b65e7b1354e2b085ae66ffac7406c44f9fd
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-11-09 15:58:49 +0100

    video chat widget base

--- dance_partner_finder/lib/video_chat_widget.dart
+++ dance_partner_finder/lib/video_chat_widget.dart
@@ -1,23 +1,184 @@
+import 'dart:convert';
+
 import 'package:dance_partner_finder/bloc/login/login_cubit.dart';
 import 'package:dance_partner_finder/bloc/video_chat/video_chat_bloc.dart';
 import 'package:dance_partner_finder/bloc/video_chat/video_chat_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:sdp_transform/sdp_transform.dart';
 
-class VideoChatWidget extends StatelessWidget {
+class VideoChatWidget extends StatefulWidget {
   final String chatParty;
 
   const VideoChatWidget({super.key, required this.chatParty});
 
   @override
+  State<VideoChatWidget> createState() => _VideoChatWidgetState();
+}
+
+class _VideoChatWidgetState extends State<VideoChatWidget> {
+  final _localVideoRenderer = RTCVideoRenderer();
+
+  final _remoteVideoRenderer = RTCVideoRenderer();
+
+  final sdpController = TextEditingController();
+
+  bool _offer = false;
+
+  RTCPeerConnection? _peerConnection;
+
+  MediaStream? _localStream;
+
+  initRenderer() async {
+    await _localVideoRenderer.initialize();
+    await _remoteVideoRenderer.initialize();
+  }
+
+  _getUserMedia() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': true,
+      'video': {
+        'facingMode': 'user',
+      }
+    };
+
+    MediaStream stream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+
+    _localVideoRenderer.srcObject = stream;
+    return stream;
+  }
+
+  _createPeerConnection() async {
+    Map<String, dynamic> configuration = {
+      "iceServers": [
+        {"url": "stun:stun.l.google.com:19302"},
+      ]
+    };
+
+    final Map<String, dynamic> offerSdpConstraints = {
+      "mandatory": {
+        "OfferToReceiveAudio": true,
+        "OfferToReceiveVideo": true,
+      },
+      "optional": [],
+    };
+
+    _localStream = await _getUserMedia();
+
+    RTCPeerConnection pc = await createPeerConnection(configuration, offerSdpConstraints);
+
+    pc.addStream(_localStream!);
+
+    pc.onIceCandidate = (e) {
+      if (e.candidate != null) {
+        print(json.encode({
+          'candidate': e.candidate.toString(),
+          'sdpMid': e.sdpMid.toString(),
+          'sdpMlineIndex': e.sdpMLineIndex,
+        }));
+      }
+    };
+
+    pc.onIceConnectionState = (e) {
+      print(e);
+    };
+
+    pc.onAddStream = (stream) {
+      print('addStream: ' + stream.id);
+      _remoteVideoRenderer.srcObject = stream;
+    };
+
+    return pc;
+  }
+
+  void _createOffer() async {
+    RTCSessionDescription description = await _peerConnection!.createOffer({'offerToReceiveVideo': 1});
+    var session = parse(description.sdp.toString());
+    print(json.encode(session));
+    _offer = true;
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _createAnswer() async {
+    RTCSessionDescription description = await _peerConnection!.createAnswer({'offerToReceiveVideo': 1});
+
+    var session = parse(description.sdp.toString());
+    print(json.encode(session));
+
+    _peerConnection!.setLocalDescription(description);
+  }
+
+  void _setRemoteDescription() async {
+    String jsonString = sdpController.text;
+    dynamic session = await jsonDecode(jsonString);
+
+    String sdp = write(session, null);
+
+    RTCSessionDescription description = RTCSessionDescription(sdp, _offer ? 'answer' : 'offer');
+    print(description.toMap());
+
+    await _peerConnection!.setRemoteDescription(description);
+  }
+
+  void _addCandidate() async {
+    String jsonString = sdpController.text;
+    dynamic session = await jsonDecode(jsonString);
+    print(session['candidate']);
+    dynamic candidate = RTCIceCandidate(session['candidate'], session['sdpMid'], session['sdpMlineIndex']);
+    await _peerConnection!.addCandidate(candidate);
+  }
+
+  @override
+  void initState() {
+    initRenderer();
+    _createPeerConnection().then((pc) {
+      _peerConnection = pc;
+    });
+    // _getUserMedia();
+    super.initState();
+  }
+
+  @override
+  void dispose() async {
+    await _localVideoRenderer.dispose();
+    sdpController.dispose();
+    super.dispose();
+  }
+
+  SizedBox videoRenderers() => SizedBox(
+        height: 210,
+        child: Row(children: [
+          Flexible(
+            child: Container(
+              key: const Key('local'),
+              margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+              decoration: const BoxDecoration(color: Colors.black),
+              child: RTCVideoView(_localVideoRenderer),
+            ),
+          ),
+          Flexible(
+            child: Container(
+              key: const Key('remote'),
+              margin: const EdgeInsets.fromLTRB(5.0, 5.0, 5.0, 5.0),
+              decoration: const BoxDecoration(color: Colors.black),
+              child: RTCVideoView(_remoteVideoRenderer),
+            ),
+          ),
+        ]),
+      );
+
+  @override
   Widget build(BuildContext context) {
     var loginCubit = context.watch<LoginCubit>();
     return BlocProvider(
-      create: (context) => VideoChatBloc(chatParty),
+      create: (context) => VideoChatBloc(widget.chatParty),
       child: BlocBuilder<VideoChatBloc, VideoChatState>(
         builder: (context, state) {
           return Scaffold(
-              appBar: AppBar(centerTitle: true, title: Text("talking with $chatParty")), body: body(context, loginCubit));
+              appBar: AppBar(centerTitle: true, title: Text("talking with ${widget.chatParty}")),
+              body: body(context, loginCubit));
         },
       ),
     );
@@ -25,6 +186,58 @@ class VideoChatWidget extends StatelessWidget {
 
   Widget body(BuildContext context, LoginCubit loginCubit) {
     var videoChatBloc = context.watch<VideoChatBloc>();
-    return !videoChatBloc.state.isLoading ? Image.asset('images/wait.gif') : Image.asset('images/wait.gif');
+    return !videoChatBloc.state.isLoading
+        ? Column(
+            children: [
+              videoRenderers(),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      child: TextField(
+                        controller: sdpController,
+                        keyboardType: TextInputType.multiline,
+                        maxLines: 4,
+                        maxLength: TextField.noMaxLength,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _createOffer,
+                        child: const Text("Offer"),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: _createAnswer,
+                        child: const Text("Answer"),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: _setRemoteDescription,
+                        child: const Text("Set Remote Description"),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ElevatedButton(
+                        onPressed: _addCandidate,
+                        child: const Text("Set Candidate"),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ],
+          )
+        : Image.asset('images/wait.gif');
   }
 }
