aa6c4ea7e5c5ab614928d293d3c83b341dca04be
Mahdi <mohammadmahdi.amini@nordnet.se>
2022-12-31 17:57:22 +0100

    api-gateway back

---
+++ api-gateway/src/main/java/wonderland/api/gateway/config/Topics.java
@@ -0,0 +1,6 @@
+package wonderland.api.gateway.config;
+
+public class Topics {
+    public static final String DANCE_PARTNER_EVENTS = "dance-partner-events";
+    public static final String DANCER_SEEKING_PARTNER_UPDATES = "dancer-seeking-partner-updates";
+}
