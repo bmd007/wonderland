892a4497a49648049268f93544d793234b0d50c8
Mahdi <bmd579@gmail.com>
2023-09-12 21:07:30 +0200

    rsocketLib

--- dance_partner_finder/lib/dance_partner_matches_page.dart
+++ dance_partner_finder/lib/dance_partner_matches_page.dart
@@ -19,6 +19,7 @@ class DancePartnerMatchesWidget extends StatelessWidget {
       create: (context) => DancerMatchAndChatBloc(loginCubit.state.email),
       child: BlocBuilder<DancerMatchAndChatBloc, DancerMatchAndChatState>(
         builder: (context, state) {
+
           if (state.isLoading) {
             return Image.asset('assets/images/wait.gif');
           } else if (state.isChattingWithSomeOne()) {
