b7b0990f64a228d2e4a1b3903343fadf6b7d4f08
Mahdi <bmd579@gmail.com>
2025-10-03 01:10:52 +0200

    JacksonConfig

---
+++ code-snippets/JacksonConfig.java
@@ -0,0 +1,26 @@
+package com.github.bmd007.telegramtopiccleanerbot.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder
+            .featuresToEnable(
+                MapperFeature.SORT_PROPERTIES_ALPHABETICALLY,
+                DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS,
+                JsonGenerator.Feature.WRITE_BIGDECIMAL_AS_PLAIN,
+                SerializationFeature.INDENT_OUTPUT
+            )
+            .featuresToDisable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+            .serializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+}
