36d70d1fa210a7389ea2e43b505f02de6cf6a720
Mahdi <bmd579@gmail.com>
2025-10-03 01:12:34 +0200

    JacksonConfig

--- code-snippets/JacksonConfig.java
+++ code-snippets/JacksonConfig.java
@@ -14,6 +14,7 @@ public class JacksonConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
         return builder -> builder
+            .modules(new JavaTimeModule())
             .featuresToEnable(
                 MapperFeature.SORT_PROPERTIES_ALPHABETICALLY,
                 DeserializationFeature.USE_BIG_DECIMAL_FOR_FLOATS,
