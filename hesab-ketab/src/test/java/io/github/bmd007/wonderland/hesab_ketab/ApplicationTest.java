e2b436426eae37a3a925f650e6abb658ed48a3b0
Mahdi <bmd579@gmail.com>
2026-04-19 20:49:11 +0200

    cleanUp

--- hesab-ketab/src/test/java/io/github/bmd007/wonderland/hesab_ketab/ApplicationTest.java
+++ hesab-ketab/src/test/java/io/github/bmd007/wonderland/hesab_ketab/ApplicationTest.java
@@ -1,29 +1,17 @@
 package io.github.bmd007.wonderland.hesab_ketab;
 
 import com.github.tomakehurst.wiremock.client.WireMock;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
-import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.test.context.ActiveProfiles;
 
-import java.sql.Blob;
-import java.time.Duration;
-import java.util.Objects;
-import java.util.concurrent.TimeUnit;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
-import static org.awaitility.Awaitility.await;
 
 @Slf4j
 @AutoConfigureWireMock
@@ -34,13 +22,8 @@ class ApplicationTest {
     @RegisterExtension
     static ContractTestExtension contractTestExtension = new ContractTestExtension();
 
-    @Value("${spring.application.name}")
-    String applicationName;
     @Autowired
     JdbcClient jdbcClient;
-
-    String instrumentBucketName = "nordnet-dummy-project-v2-instruments-dump";
-    String instrumentMappingBucketName = "nordnet-dummy-project-v1-instrument-mappings-dump";
 
     @AfterEach
     void afterEach() {
